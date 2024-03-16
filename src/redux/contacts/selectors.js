0cfd6df37800b06466cf3776cc3d98fb7390e032
k1nderko <107281239+k1nderko@users.noreply.github.com>
2024-03-17 00:20:40 +0300

    Update selectors.js

--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,2 +1,15 @@
-export const getContacts = state => state.contacts.contacts;
+import { createSelector } from 'reselect';
+
+export const getContacts = state => state.contacts.items;
 export const getFilter = state => state.filter;
+export const getLoading = state => state.contacts.isLoading;
+export const getError = state => state.contacts.error;
+export const getVisibleContacts = createSelector(
+  [getContacts, getFilter],
+  (items, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
