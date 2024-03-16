542c6afac93793531d66bb3e875f64f94a91e22f
k1nderko <107281239+k1nderko@users.noreply.github.com>
2024-03-17 00:17:02 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,39 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 
 import contactsReducer from './contacts/contactsSlice';
 import filterReducer from './contacts/filterSlice';
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage: storage,
-};
-
 const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPersistConfig, contactsReducer),
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
 
-let persistor = persistStore(store);
-
-const rootStore = { store, persistor };
+const rootStore = { store };
 
 export default rootStore;
